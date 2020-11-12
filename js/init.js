7f053481dc3cee01ec1876a4d45df4a6badeb412
Austin Cheng <austinbicheng@gmail.com>
2020-11-11 21:33:30 -0800

    2.0, updated materialize and updated the entire website to accomidate changes

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $(".dropdown-trigger").dropdown({hover: true, alignment: "left", constrainWidth: false});
+    $(document).ready(function(){$('.carousel').carousel();
+  });
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
