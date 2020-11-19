096ecf48a12cdce1e7d768afdd4a2c9cd803e8d8
Austin Cheng <austinbicheng@gmail.com>
2020-11-19 02:00:17 -0800

    large update! Adding customized pages for each project. Vast finished

--- js/init.js
+++ js/init.js
@@ -3,6 +3,10 @@
     $('.parallax').parallax();
     $(".dropdown-trigger").dropdown({hover: true, alignment: "left", constrainWidth: false});
     $(document).ready(function(){$('.carousel').carousel();
+    $('.carousel.carousel-slider').carousel({
+       fullWidth: true,
+       indicators: true
+     });
   });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
