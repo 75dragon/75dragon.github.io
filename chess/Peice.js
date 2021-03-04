6223ab9e7a66717baa90ac37fe29acfa0b95be7e
Austin Cheng <austinbicheng@gmail.com>
2021-03-03 17:06:22 -0800

    movement structure implimented

--- chess/Peice.js
+++ chess/Peice.js
@@ -43,6 +43,23 @@ class King extends Piece
 		super(col, row, isWhite, "King", img);
 		this.letter = "K";
 	}
+
+	moves(board)
+	{
+		var moves = new Array(64).fill(false);
+		for (var c = 0; c < 8; c++)
+		{
+			for (var r = 0; r < 8; r++)
+			{
+				if (Math.abs(this.col - c) <= 1 && Math.abs(this.row - r) <= 1)
+				{
+					if (!board.squareOccupied(c, r) || (board.getPiece(c, r).isWhite != this.isWhite))
+					moves[c + 8 * r] = true;
+				}
+			}
+		}
+		return moves;
+	}
 }
 
 class Queen extends Piece
@@ -51,6 +68,112 @@ class Queen extends Piece
 	{
 		super(col, row, isWhite, "Queen", img);
 		this.letter = "Q";
+	}
+
+	moves(board)
+	{
+		var moves = new Array(64).fill(false);
+		var c = this.col - 1;
+		var r = this.row;
+		//go left
+		while (c >= 0 && (!board.squareOccupied(c, r) || (board.getPiece(c, r).isWhite != this.isWhite)))
+		{
+			moves[c + 8 * r] = true;
+			if (board.getPiece(c, r).isWhite != this.isWhite)
+			{
+				break;
+			}
+			c = c - 1
+		}
+		c = this.col - 1;
+		r = this.row + 1;
+		//go left down
+		while (c >= 0 && r <= 7 && (!board.squareOccupied(c, r) || (board.getPiece(c, r).isWhite != this.isWhite)))
+		{
+			moves[c + 8 * r] = true;
+			if (board.getPiece(c, r).isWhite != this.isWhite)
+			{
+				break;
+			}
+			c = c - 1;
+			r = r + 1;
+		}
+		c = this.col;
+		r = this.row + 1;
+		//go down
+		while (r <= 7 && (!board.squareOccupied(c, r) || (board.getPiece(c, r).isWhite != this.isWhite)))
+		{
+			moves[c + 8 * r] = true;
+			if (board.getPiece(c, r).isWhite != this.isWhite)
+			{
+				break;
+			}
+			r = r + 1;
+		}
+		c = this.col + 1;
+		r = this.row + 1;
+		//go right down
+		while (c <= 7 && r <= 7 && (!board.squareOccupied(c, r) || (board.getPiece(c, r).isWhite != this.isWhite)))
+		{
+			moves[c + 8 * r] = true;
+			if (board.getPiece(c, r).isWhite != this.isWhite)
+			{
+				break;
+			}
+			c = c + 1;
+			r = r + 1;
+		}
+		c = this.col + 1;
+		r = this.row;
+		//go right
+		while (c <= 7 && (!board.squareOccupied(c, r) || (board.getPiece(c, r).isWhite != this.isWhite)))
+		{
+			moves[c + 8 * r] = true;
+			if (board.getPiece(c, r).isWhite != this.isWhite)
+			{
+				break;
+			}
+			c = c + 1
+		}
+		c = this.col + 1;
+		r = this.row - 1;
+		//go right up
+		while (c <= 7 && r >= 0 && (!board.squareOccupied(c, r) || (board.getPiece(c, r).isWhite != this.isWhite)))
+		{
+			moves[c + 8 * r] = true;
+			if (board.getPiece(c, r).isWhite != this.isWhite)
+			{
+				break;
+			}
+			c = c + 1
+			r = r - 1
+		}
+		c = this.col;
+		r = this.row - 1;
+		//go up
+		while (r >= 0 && (!board.squareOccupied(c, r) || (board.getPiece(c, r).isWhite != this.isWhite)))
+		{
+			moves[c + 8 * r] = true;
+			if (board.getPiece(c, r).isWhite != this.isWhite)
+			{
+				break;
+			}
+			r = r - 1;
+		}
+		c = this.col - 1;
+		r = this.row - 1;
+		//go up left
+		while (c >= 0 && r >= 0 && (!board.squareOccupied(c, r) || (board.getPiece(c, r).isWhite != this.isWhite)))
+		{
+			moves[c + 8 * r] = true;
+			if (board.getPiece(c, r).isWhite != this.isWhite)
+			{
+				break;
+			}
+			c = c - 1
+			r = r - 1
+		}
+		return moves;
 	}
 }
 
@@ -61,6 +184,60 @@ class Rook extends Piece
 		super(col, row, isWhite, "Rook", img);
 		this.letter = "R";
 	}
+
+	moves(board)
+	{
+		var moves = new Array(64).fill(false);
+		var c = this.col - 1;
+		var r = this.row;
+		//go left
+		while (c >= 0 && (!board.squareOccupied(c, r) || (board.getPiece(c, r).isWhite != this.isWhite)))
+		{
+			moves[c + 8 * r] = true;
+			if (board.getPiece(c, r).isWhite != this.isWhite)
+			{
+				break;
+			}
+			c = c - 1
+		}
+		c = this.col;
+		r = this.row + 1;
+		//go down
+		while (r <= 7 && (!board.squareOccupied(c, r) || (board.getPiece(c, r).isWhite != this.isWhite)))
+		{
+			moves[c + 8 * r] = true;
+			if (board.getPiece(c, r).isWhite != this.isWhite)
+			{
+				break;
+			}
+			r = r + 1;
+		}
+		c = this.col + 1;
+		r = this.row;
+		//go right
+		while (c <= 7 && (!board.squareOccupied(c, r) || (board.getPiece(c, r).isWhite != this.isWhite)))
+		{
+			moves[c + 8 * r] = true;
+			if (board.getPiece(c, r).isWhite != this.isWhite)
+			{
+				break;
+			}
+			c = c + 1
+		}
+		c = this.col;
+		r = this.row - 1;
+		//go up
+		while (r >= 0 && (!board.squareOccupied(c, r) || (board.getPiece(c, r).isWhite != this.isWhite)))
+		{
+			moves[c + 8 * r] = true;
+			if (board.getPiece(c, r).isWhite != this.isWhite)
+			{
+				break;
+			}
+			r = r - 1;
+		}
+		return moves;
+	}
 }
 
 class Bishop extends Piece
@@ -70,14 +247,93 @@ class Bishop extends Piece
 		super(col, row, isWhite, "Bishop", img);
 		this.letter = "B";
 	}
+
+	moves(board)
+	{
+		var moves = new Array(64).fill(false);
+		var c = this.col - 1;
+		var r = this.row + 1;
+		//go left down
+		while (c >= 0 && r <= 7 && (!board.squareOccupied(c, r) || (board.getPiece(c, r).isWhite != this.isWhite)))
+		{
+			moves[c + 8 * r] = true;
+			if (board.getPiece(c, r).isWhite != this.isWhite)
+			{
+				break;
+			}
+			c = c - 1;
+			r = r + 1;
+		}
+		c = this.col + 1;
+		r = this.row + 1;
+		//go right down
+		while (c <= 7 && r <= 7 && (!board.squareOccupied(c, r) || (board.getPiece(c, r).isWhite != this.isWhite)))
+		{
+			moves[c + 8 * r] = true;
+			if (board.getPiece(c, r).isWhite != this.isWhite)
+			{
+				break;
+			}
+			c = c + 1;
+			r = r + 1;
+		}
+
+		c = this.col + 1;
+		r = this.row - 1;
+		//go right up
+		while (c <= 7 && r >= 0 && (!board.squareOccupied(c, r) || (board.getPiece(c, r).isWhite != this.isWhite)))
+		{
+			moves[c + 8 * r] = true;
+			if (board.getPiece(c, r).isWhite != this.isWhite)
+			{
+				break;
+			}
+			c = c + 1
+			r = r - 1
+		}
+
+		c = this.col - 1;
+		r = this.row - 1;
+		//go up left
+		while (c >= 0 && r >= 0 && (!board.squareOccupied(c, r) || (board.getPiece(c, r).isWhite != this.isWhite)))
+		{
+			moves[c + 8 * r] = true;
+			if (board.getPiece(c, r).isWhite != this.isWhite)
+			{
+				break;
+			}
+			c = c - 1
+			r = r - 1
+		}
+		return moves;
+	}
 }
 
 class Knight extends Piece
 {
-	constructor(col,row,isWhite, img)
+	constructor(col, row, isWhite, img)
 	{
 		super(col, row, isWhite, "Knight", img);
 		this.letter = "Kn";
+	}
+
+	moves(board)
+	{
+		var moves = new Array(64).fill(false);
+		var colArray = [1,1,2,2,-1,-1,-2,-2]
+		var rowArray = [2,-2,1,-1,2,-2,1,-1]
+		var c = 0;
+		var r = 0;
+		for (var i = 0; i < 8; i++)
+		{
+			c = this.col + colArray[i]
+			r = this.row + rowArray[i] 
+			if (c >= 0 && c <= 7 && r >= 0 && r <= 7 && (!board.squareOccupied(c, r) || (board.getPiece(c, r).isWhite != this.isWhite)))
+			{
+				moves[c + 8 * r] = true;
+			}
+		}
+		return moves;
 	}
 }
 
@@ -87,5 +343,45 @@ class Pawn extends Piece
 	{
 		super(col, row, isWhite, "Pawn", img);
 		this.letter = "P";
+		this.hasNotMoved = true;
+	}
+
+	moves(board)
+	{
+		var moves = new Array(64).fill(false);
+		var direction = 1;
+		if (this.isWhite)
+		{
+			direction = -1;
+		}
+		var c = this.col;
+		var r = this.row + direction;
+		if (r >= 0 && r <= 7 && !board.squareOccupied(c, r))
+		{
+			moves[c + 8 * r] = true;
+			this.hasNotMoved = false;
+		}
+		c = this.col;
+		r = this.row + direction * 2;
+		if (this.hasNotMoved && r >= 0 && r <= 7 && !board.squareOccupied(c, r))
+		{
+			moves[c + 8 * r] = true;
+			this.hasNotMoved = false;
+		}
+		c = this.col - 1;
+		r = this.row + direction;
+		if (c >= 0 && c <= 7 && r >= 0 && r <= 7 && board.squareOccupied(c, r) && (board.getPiece(c, r).isWhite != this.isWhite))
+		{
+			moves[c + 8 * r] = true;
+			this.hasNotMoved = false;
+		}
+		c = this.col + 1;
+		r = this.row + direction;
+		if (c >= 0 && c <= 7 && r >= 0 && r <= 7 && board.squareOccupied(c, r) && (board.getPiece(c, r).isWhite != this.isWhite))
+		{
+			moves[c + 8 * r] = true;
+			this.hasNotMoved = false;
+		}
+		return moves;
 	}
 }
